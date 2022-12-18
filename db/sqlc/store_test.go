b4770e2cd96ebacec646d499c42b211e5d6cdfb6
arya <arya@accelbyte.net>
2022-12-19 00:49:25 +0700

    feat: clean way db transaction and avoid deadlock

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"fmt"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
@@ -15,6 +16,9 @@ func TestTransferTx(t *testing.T) {
 	// waiting database transaction needed to be careful. it's easy to write but also easily become a nightmare
 	// handle it with go routine/concurrent
 
+	// logging
+	fmt.Println("before : ", "accountA", accountA.Balance, "accountB", accountB.Balance)
+
 	// run n concurrent transfer transaction
 	n := 5
 	amount := int64(10)
@@ -25,7 +29,8 @@ func TestTransferTx(t *testing.T) {
 
 	for i := 0; i < n; i++ {
 		go func() { // start go routine
-			result, err := store.TransferTx(context.Background(), TransferTxParams{
+			ctx := context.Background()
+			result, err := store.TransferTx(ctx, TransferTxParams{
 				FromAccountID: accountA.ID,
 				ToAccountID:   accountB.ID,
 				Amount:        amount,
@@ -38,6 +43,7 @@ func TestTransferTx(t *testing.T) {
 	}
 
 	// check results
+	existed := make(map[int]bool)
 	for i := 0; i < n; i++ {
 		// recipe from channel
 		err := <-errs
@@ -79,6 +85,101 @@ func TestTransferTx(t *testing.T) {
 		_, err = store.GetEntry(context.Background(), toEntry.ID)
 		require.NoError(t, err)
 
-		// TODO: check account balance
+		// check accounts
+		fromAccount := result.FromAccount
+		require.NotEmpty(t, fromAccount)
+		require.Equal(t, accountA.ID, fromAccount.ID)
+
+		toAccount := result.ToAccount
+		require.NotEmpty(t, toAccount)
+		require.Equal(t, accountB.ID, toAccount.ID)
+
+		//check account's balance
+
+		// logging
+		fmt.Println("transaction : ", "from", fromAccount.Balance, "to", toAccount.Balance)
+		diffA := accountA.Balance - fromAccount.Balance
+		diffB := toAccount.Balance - accountB.Balance
+		require.Equal(t, diffA, diffB)
+		require.True(t, diffA > 0)
+		require.True(t, diffA%amount == 0) // 1 = amount, 2 * amount .... n * amount
+
+		k := int(diffA / amount)
+		require.True(t, k >= 1 && k <= n)
+		require.NotContains(t, existed, k)
+		existed[k] = true
 	}
+
+	// check final updated balance
+	updatedAccountA, err := testQueries.GetAccount(context.Background(), accountA.ID)
+	require.NoError(t, err)
+
+	updatedAccountB, err := testQueries.GetAccount(context.Background(), accountB.ID)
+	require.NoError(t, err)
+
+	// logging
+	fmt.Println("after : ", "accountA", updatedAccountA.Balance, "accountB", updatedAccountB.Balance)
+	require.Equal(t, accountA.Balance-int64(n)*amount, updatedAccountA.Balance)
+	require.Equal(t, accountB.Balance+int64(n)*amount, updatedAccountB.Balance)
+}
+
+func TestTransferTxDeacLock(t *testing.T) {
+	store := NewStore(testDB)
+
+	accountA := createRandomAccount(t)
+	accountB := createRandomAccount(t)
+
+	// waiting database transaction needed to be careful. it's easy to write but also easily become a nightmare
+	// handle it with go routine/concurrent
+
+	// logging
+	fmt.Println("before : ", "accountA", accountA.Balance, "accountB", accountB.Balance)
+
+	// run n concurrent transfer transaction
+	n := 10
+	amount := int64(10)
+
+	// verified error and result, send them back to go routine
+	errs := make(chan error)
+
+	for i := 0; i < n; i++ {
+		fromAccountId := accountA.ID
+		toAccountId := accountA.ID
+
+		if i%2 == 1 {
+			fromAccountId = accountB.ID
+			toAccountId = accountB.ID
+		}
+		go func() { // start go routine
+			ctx := context.Background()
+			_, err := store.TransferTx(ctx, TransferTxParams{
+				FromAccountID: fromAccountId,
+				ToAccountID:   toAccountId,
+				Amount:        amount,
+			})
+
+			// send to channel
+			errs <- err
+		}()
+	}
+
+	// check results
+	for i := 0; i < n; i++ {
+		// recipe from channel
+		err := <-errs
+		require.NoError(t, err)
+
+	}
+
+	// check final updated balance
+	updatedAccountA, err := testQueries.GetAccount(context.Background(), accountA.ID)
+	require.NoError(t, err)
+
+	updatedAccountB, err := testQueries.GetAccount(context.Background(), accountB.ID)
+	require.NoError(t, err)
+
+	// logging
+	fmt.Println("after : ", "accountA", updatedAccountA.Balance, "accountB", updatedAccountB.Balance)
+	require.Equal(t, accountA.Balance, updatedAccountA.Balance)
+	require.Equal(t, accountB.Balance, updatedAccountB.Balance)
 }
